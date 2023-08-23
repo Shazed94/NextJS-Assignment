77cd866cb27d948e80ee7bf93b96c178b18b83f0
Shazed94 <ashraf.shazed@gmail.com>
2023-08-24 03:09:14 +0600

    module 12 assignment

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,9 @@ const Header = () => {
             <div className='container mx-auto flex justify-between py-6'>
                 <h3>Next13</h3>
                 <div className='flex space-x-3 md:space-x-9 list-none'>
+                    <Link href="/">
+                        <li className='uppercase text-blue-400'>Home</li>
+                    </Link>
                     <Link href="/blog">
                         <li className='uppercase text-blue-400'>Blog</li>
                     </Link>
