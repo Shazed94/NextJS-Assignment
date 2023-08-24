ba6f21894677605445ff619c0ba913c46cde34bc
Shazed94 <ashraf.shazed@gmail.com>
2023-08-24 17:22:23 +0600

    about

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="h-[calc(100vh-126px)] flex justify-center items-center">
+      <div className="bg-gray-200/50 p-8 rounded-3xl">
 
-export default About
+        <div className="w-24 h-24 mb-8 mx-auto">
+          <img
+            src="./rafi.JPG"
+            alt=""
+            className="w-full h-full rounded-full ring-2 ring-offset-4 ring-slate-700"
+          />
+        </div>
+        <h3 className="font-semibold text-2xl mb-3">Md. Shazed Ashraf</h3>
+        <p>Front-End Developer</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- app/api/getNewestPost.js
+++ app/api/getNewestPost.js
@@ -1,5 +1,6 @@
 
 export default async function getNewestPost() {
+
     const res = await fetch("https://basic-blog.teamrabbil.com/api/post-newest")
     if (!res.ok) {
         throw new Error("Something went wrong!")
