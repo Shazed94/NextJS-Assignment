94a72ab9ac50f40d103d73e77e929f16a124dc50
Shazed94 <ashraf.shazed@gmail.com>
2023-08-31 11:53:34 +0600

    update

--- app/lib/setCookie.js
+++
@@ -1,9 +0,0 @@
-
-export default async function setCookie() {
-
-    const res = await fetch("http://localhost:3000/api/set-cookie")
-    if (!res.ok) {
-        throw new Error("Something went wrong!")
-    }
-    return res.json()
-}
--- app/page.js
+++ app/page.js
@@ -1,25 +1,30 @@
 "use client";
-import Link from "next/link";
-import setCookie from "./lib/setCookie"
+
+import { toast, ToastContainer } from "react-toastify";
+
 export default function Home() {
 
   const handleSetCookie = async () => {
-    console.log("hi")
-    const response = await fetch('http://localhost:3000/api/cookies/set-cookie')
-    if (!response.ok) {
-      throw new Error(response.status)
+    const res = await fetch("http://localhost:3000/api/cookies/set-cookie")
+    if (!res.ok) {
+      throw new Error("Something went wrong!")
     }
-    return response.json()
-    // setCookie()
+    toast.success('Cookie added successfully ', {
+      position: toast.POSITION.TOP_RIGHT,
+      hideProgressBar: "true",
+      autoClose:2500
+    });
+    return res.json()
   }
   return (
     <>
       <div className="container mx-auto my-7">
         <div className="flex justify-center">
+          <ToastContainer />
+
           <button onClick={handleSetCookie} className="bg-green-600 text-white px-7 py-3">
             Set cookie
           </button>
-
         </div>
       </div >
     </>
