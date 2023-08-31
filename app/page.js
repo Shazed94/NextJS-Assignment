c7ba7fa17952029cf5992dfb4b8f41cf1a74e3b8
Shazed94 <ashraf.shazed@gmail.com>
2023-08-31 11:22:47 +0600

    update

--- app/page.js
+++ app/page.js
@@ -3,23 +3,21 @@ import Link from "next/link";
 import setCookie from "./lib/setCookie"
 export default function Home() {
 
-  const handleSetCookie = () => {
-
-    // const response = await fetch('http://localhost:3000/api/set-cookie')
-    // if (!response.ok) {
-    //   throw new Error(response.status)
-    // }
-    // return response.json()
-    setCookie()
+  const handleSetCookie = async () => {
+    console.log("hi")
+    const response = await fetch('http://localhost:3000/api/cookies/set-cookie')
+    if (!response.ok) {
+      throw new Error(response.status)
+    }
+    return response.json()
+    // setCookie()
   }
   return (
     <>
       <div className="container mx-auto my-7">
         <div className="flex justify-center">
           <button onClick={handleSetCookie} className="bg-green-600 text-white px-7 py-3">
-            {/* <Link href={'/api/cookies/set-cookie'}> */}
-              Set cookie
-            {/* </Link> */}
+            Set cookie
           </button>
 
         </div>
