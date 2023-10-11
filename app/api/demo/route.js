cda9a6bf8c2e6b37ce1d8b38ae92a1fd17fad3cb
Shazed94 <ashraf.shazed@gmail.com>
2023-10-11 14:21:50 +0600

    update

--- app/api/demo/route.js
+++ app/api/demo/route.js
@@ -1,11 +1,78 @@
-import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
-import React from 'react'
-
-export async function GET() {
-
-    const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client'
 
 
-    return NextResponse.json({ status: "success" })
+export async function POST(req, res) {
+    try {
+        //  ! Fix serialize a BigInt
+        BigInt.prototype.toJSON = function () {
+            return this.toString();
+        }
+
+        let formData = await req.formData()
+
+        const prisma = new PrismaClient()
+        let result = await prisma.user.create({
+            data: {
+                firstName: formData.get("firstName"),
+                middleName: formData?.get("middleName"),
+                lastName: formData.get("lastName"),
+                mobile: formData.get("mobile"),
+                email: formData.get("middleName"),
+                passwordHash: formData.get("passwordHash"),
+                intro: formData.get("intro"),
+                profile: formData.get("profile"),
+                post: {
+                    create: {
+                        title: "post 1",
+                        metaTitle: "post_1",
+                        slug: "post_slug",
+                        summery: "post_summery",
+                        published: 1,
+                        content: "post content",
+                        parentId:"3"
+                    }
+                }
+            }
+
+        })
+        console.log(result)
+        return NextResponse.json({
+            status: "success", data: result
+        })
+    } catch (error) {
+        return NextResponse.json({
+            status: "success", data: error.toString()
+        })
+    }
+}
+export async function PUT(req, res) {
+    try {
+        //  ! Fix serialize a BigInt
+        BigInt.prototype.toJSON = function () {
+            return this.toString();
+        }
+
+        let formData = await req.formData()
+
+        const prisma = new PrismaClient()
+        let result = await prisma.post.create({
+            data: {
+                title: formData.get("title"),
+                metaTitle: formData.get("metaTitle"),
+                slug: formData.get("slug"),
+                published: formData.get("published"),
+                content: formData.get("content"),
+            }
+        })
+
+        return NextResponse.json({
+            status: "success", data: result
+        })
+    }
+    catch (error) {
+        return NextResponse.json({
+            status: "success", data: error.toString()
+        })
+    }
 }
---
+++ prisma/migrations/20231011075251_new/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `user` MODIFY `middleName` VARCHAR(50) NULL;
